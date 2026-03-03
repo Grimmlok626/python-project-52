a088cf8c437bf01857232ecb9454a5d822380ba3
grimmlok <Shinigami_626@mail.ru>
2026-03-03 09:22:18 +0300

    Второй шаг

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from django.shortcuts import render
+from . import views
 
 def home(request):
     return render(request, "home.html")
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('register/', views.register, name='register'),
+    path('users/', views.users_list, name='users'),
 ]
