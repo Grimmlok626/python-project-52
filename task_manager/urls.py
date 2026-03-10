0618cdcf7b71b9f6a1760ca1fc31e11beb793d00
grimmlok <Shinigami_626@mail.ru>
2026-03-10 16:27:12 +0300

    Третий шаг

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,11 @@
+from django.urls import path, include
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from . import views
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('users/', include('task_manager.users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
