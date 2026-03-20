8c3902e71d2b2617039ef31c3cdf787024a0294f
grimmlok <Shinigami_626@mail.ru>
2026-03-20 19:37:45 +0300

    Последний шаг

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key-for-dev")
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "webserver",
-    "python-project-52-7l3z.onrender.com",
-    ".onrender.com",
-]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
