c8fc4c7d845bd7d3359a608cd2a6ed4751860e87
grimmlok <Shinigami_626@mail.ru>
2026-03-21 13:35:56 +0300

    Понеслись эксперименты 1

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from task_manager import views
+from .views import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
