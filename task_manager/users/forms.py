bf5573fd2cb830b920bfd9989309b82dbf2c0d34
grimmlok <Shinigami_626@mail.ru>
2026-03-27 23:24:02 +0300

    Понеслись эксперименты 6

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
-        labels = {
-            'username': 'Имя пользователя',
-            'password1': 'Пароль',
-            'password2': 'Подтверждение пароля',
-        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].label = 'Подтверждение пароля'
