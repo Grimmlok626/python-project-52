a7ed4c8eeb3c2a1d985cd63f018f917ff2e000e0
grimmlok <Shinigami_626@mail.ru>
2026-03-30 15:09:43 +0300

    Понеслись эксперименты 8

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,14 +1,26 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(label='Имя', max_length=150, required=True)
+    last_name = forms.CharField(label='Фамилия', max_length=150, required=True)
+
     class Meta:
         model = User
-        fields = ('username', 'password1', 'password2')
-    
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].label = 'Имя пользователя'
         self.fields['password1'].label = 'Пароль'
         self.fields['password2'].label = 'Подтверждение пароля'
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
